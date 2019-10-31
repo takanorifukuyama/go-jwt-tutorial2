7fd6ae2904dd7c17dd6eb29490458d8f431e0d2f
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-11-01 05:01:08 +0900

    manually testing is success

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 [参考サイト](https://blog.motikan2010.com/entry/2017/05/12/jwt-go%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B)
 
+test
+```
+http localhost:8080/api
+```
 
+```
+http localhost:8080/api/private Authorization:YOUR_TOKEN
+```
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -10,7 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var secretKey = "75c92a074c341e9964329c0550c2673730ed8479c885c43122c90a2843177d5ef21cb50cfadcccb20aeb730487c11e09ee4dbbb02387242ef264e74cbee97213"
+var secretKey = os.Getenv("TOKEN_SECRET_KEY")
 
 func main() {
 	r := gin.Default()
@@ -21,7 +22,7 @@ func main() {
 		*/
 		token := jwt.New(jwt.GetSigningMethod("HS256"))
 		token.Claims = jwt.MapClaims{
-			"user": "ゲスト",
+			"user": "Takanori",
 			"exp":  time.Now().Add(time.Hour * 1).Unix(),
 		}
 
@@ -30,9 +31,10 @@ func main() {
 		*/
 		tokenString, err := token.SignedString([]byte(secretKey))
 		if errors.Is(err, nil) {
+			c.JSON(200, gin.H{"token": tokenString})
+		} else {
 			c.JSON(500, gin.H{"message": "Could not generate token"})
 		}
-		c.JSON(200, gin.H{"token": tokenString})
 	})
 
 	r.GET("/api/private", func(c *gin.Context) {
@@ -48,11 +50,11 @@ func main() {
 			},
 		)
 		if errors.Is(err, nil) {
-			c.JSON(401, gin.H{"error": fmt.Sprint(err)})
-		} else {
 			claims := token.Claims.(jwt.MapClaims)
 			msg := fmt.Sprintf("Hello!! %s!!", claims["user"])
 			c.JSON(200, gin.H{"message": msg})
+		} else {
+			c.JSON(401, gin.H{"error": fmt.Sprint(err)})
 		}
 	})
 	r.Run(":8080")
